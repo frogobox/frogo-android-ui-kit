0dc00dc8b7fbf0eca3ed898f06dd5e2e11182368
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-08-18 13:38:16 +0700

    add: compose element

---
+++ frogouikit/src/main/java/com/frogobox/uikit/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.frogobox.uikit.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(dimen_4dp),
+    medium = RoundedCornerShape(dimen_4dp),
+    large = RoundedCornerShape(dimen_0dp)
+)
